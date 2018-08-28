b0064bcbd3618453450862325ee526d80ee42ad8
Andre Oliveira <andreoliveira.adv@hotmail.com>
2018-08-28 11:46:11 -0400

    Version 2 - André Oliveira - le 28.08.2018

--- app/src/main/java/com/example/andreoliveira/biblio/util/Encoding.java
+++ app/src/main/java/com/example/andreoliveira/biblio/util/Encoding.java
@@ -4,12 +4,12 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class Encoding {
-
-    public static String encoding(String password) throws NoSuchAlgorithmException {
+public class Encoding
+{
+    public static String encoding(String password) throws NoSuchAlgorithmException
+    {
         MessageDigest messageDigest = MessageDigest.getInstance("MD5");
         messageDigest.update(password.getBytes(),0,password.length());
         return new BigInteger(1,messageDigest.digest()).toString(16);
     }
-
 }
